c09c925d8af248818dd6fc0208ab72e70065e15d
Temptier <mmmcoc3@gmail.com>
2025-11-07 20:36:20 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,12 +10,12 @@ import {
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCcZa-fnSwdD36rB_DAR-SSfFlzH2fqcPc",
+  authDomain: "lordninetimer.firebaseapp.com",
+  projectId: "lordninetimer",
+  storageBucket: "lordninetimer.firebasestorage.app",
+  messagingSenderId: "462837939255",
+  appId: "1:462837939255:web:dee141d630d5d9b94a53b2"
 };
 
 const app = initializeApp(firebaseConfig);
