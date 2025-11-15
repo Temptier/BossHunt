34938375aa4cf7698fc6b88e4e71fdc51a2187ab
Temptier <mmmcoc3@gmail.com>
2025-11-16 01:51:25 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/repo-name/', // <--- Replace with your repo name
   plugins: [react()],
   server: {
     host: true,
     allowedHosts: true,
   },
-})
+});
