5b5507043fa9a823f8cbbdc5b66e864fb5b5cc49
Temptier <mmmcoc3@gmail.com>
2025-11-16 02:21:58 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/repo-name/', // <--- Replace with your repo name
+  base: '/BossHunt/', // <--- Replace with your repo name
   plugins: [react()],
   server: {
     host: true,
